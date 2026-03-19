0d9b3ee91e804e49f852abc90b23defb1bcf03c8
yujun <2291876007@qq.com>
2026-03-19 17:06:57 +0800

    feat: add 1.6 history node to knowledge map SVG

--- book/searcher-c2a407aa.js
+++ book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-ffb213ae.js',
+                path_to_root + 'searchindex-f226d5d7.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
